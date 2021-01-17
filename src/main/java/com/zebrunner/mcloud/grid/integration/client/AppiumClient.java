1f8f83a498c12e709631e9a6120fb863c5214acd
vdelendik <vadim.delendik@gmail.com>
2021-01-17 16:37:05 +0300

    updated copyright info

--- src/main/java/com/zebrunner/mcloud/grid/integration/client/AppiumClient.java
+++ src/main/java/com/zebrunner/mcloud/grid/integration/client/AppiumClient.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2013-2021 Qaprosoft (http://www.qaprosoft.com).
+ * Copyright 2018-2021 Zebrunner (https://zebrunner.com/).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
