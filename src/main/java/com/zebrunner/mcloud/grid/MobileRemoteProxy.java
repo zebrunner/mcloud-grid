c2002526759cb7228118d8a1c0de13277e644c18
vdelendik <vadim.delendik@gmail.com>
2021-02-18 20:52:13 +0300

    #38: Set video recroding limit to 1 hour for both android and ios

--- src/main/java/com/zebrunner/mcloud/grid/MobileRemoteProxy.java
+++ src/main/java/com/zebrunner/mcloud/grid/MobileRemoteProxy.java
@@ -227,9 +227,9 @@ public class MobileRemoteProxy extends DefaultRemoteProxy {
     private void startRecording(String sessionId, String appiumUrl, TestSession session) {
         // TODO: organize overriding video options via capabilitities. Think about putting all options.* one more time if exist
         Map<String, String> options = new HashMap<>();
+        options.put("timeLimit", "3600"); // 1 hour as maximal video recording duration
         if (Platform.ANDROID.equals(Platform.fromCapabilities(session.getRequestedCapabilities()))) {
             options.put("forceRestart", "true");
-            options.put("timeLimit", "1800");
             options.put("bitRate", "1000000");
             options.put("bugReport", "true");
         } else if (Platform.IOS.equals(Platform.fromCapabilities(session.getRequestedCapabilities()))) {
