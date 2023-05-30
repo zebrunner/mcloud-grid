4bdf70a02550eed5a2eb86c6047e4f57b958b5ce
vdelendik <vadim.delendik@gmail.com>
2023-05-30 16:05:06 +0200

    fixed unit tests and removed support of the caps without appium prefix for non w3c

--- src/main/java/com/zebrunner/mcloud/grid/MobileCapabilityMatcher.java
+++ src/main/java/com/zebrunner/mcloud/grid/MobileCapabilityMatcher.java
@@ -32,18 +32,17 @@ public class MobileCapabilityMatcher extends DefaultCapabilityMatcher {
     private static final Logger LOGGER = Logger.getLogger(MobileCapabilityMatcher.class.getName());
     
     private static final String PLATFORM_NAME = "platformName";
-    //TODO: remove APPIUM_ prefix as useless.
-    private static final String APPIUM_PLATFORM_VERSION = "appium:platformVersion";
-    private static final String APPIUM_DEVICE_NAME = "appium:deviceName";
-    private static final String APPIUM_DEVICE_TYPE = "appium:deviceType";
+    private static final String PLATFORM_VERSION = "appium:platformVersion";
+    private static final String DEVICE_NAME = "appium:deviceName";
+    private static final String DEVICE_TYPE = "appium:deviceType";
     private static final String APPIUM_UDID = "appium:udid";
 
     @Override
     public boolean matches(Map<String, Object> nodeCapability, Map<String, Object> requestedCapability) {
         LOGGER.finest("requestedCapability: " + requestedCapability);
         
-        if (requestedCapability.containsKey(PLATFORM_NAME) || requestedCapability.containsKey(APPIUM_PLATFORM_VERSION)
-                || requestedCapability.containsKey(APPIUM_DEVICE_NAME)
+        if (requestedCapability.containsKey(PLATFORM_NAME) || requestedCapability.containsKey(PLATFORM_VERSION)
+                || requestedCapability.containsKey(DEVICE_NAME)
                 || requestedCapability.containsKey(APPIUM_UDID)) {
             // Mobile-based capabilities
             LOGGER.fine("Using extensionCapabilityCheck matcher.");
@@ -66,21 +65,11 @@ public class MobileCapabilityMatcher extends DefaultCapabilityMatcher {
      */
 	private boolean extensionCapabilityCheck(Map<String, Object> nodeCapability,
             Map<String, Object> requestedCapability) {
-	    final String APPIUM_PREFIX = "appium:";
 
-	    //w3c formatted caps request:
-        // 13:21:50.524 FINEST [MobileCapabilityMatcher.matches] - requestedCapability: {appium:enableVideo=true, appium:locale=US,
-        // pageLoadStrategy=eager, appium:language=en, appium:remoteAppsCacheLimit=0, appium:noSign=true, appium:automationName=UIAutomator2,
-        // appium:enableLog=true, appium:autoGrantPermissions=true, appium:deviceName=Nokia_3, platformName=android,
-        // appium:app=https://qaprosoft.s3-us-west-2.amazonaws.com/carinademoexample.apk}
-        for (String key : requestedCapability.keySet()) {
-            String expectedValue = requestedCapability.get(key) != null ? requestedCapability.get(key).toString()
+        for (String nodeKey : requestedCapability.keySet()) {
+            String expectedValue = requestedCapability.get(nodeKey) != null ? requestedCapability.get(nodeKey).toString()
                     : null;
             
-            // cut w3c "appium:" prefix if any
-            expectedValue = expectedValue.replace(APPIUM_PREFIX, "");
-
-            String nodeKey = key.replace(APPIUM_PREFIX, "");
             String actualValue = (nodeCapability.containsKey(nodeKey) && nodeCapability.get(nodeKey) != null)
                     ? nodeCapability.get(nodeKey).toString()
                     : null;
@@ -93,7 +82,7 @@ public class MobileCapabilityMatcher extends DefaultCapabilityMatcher {
                         return false;
                     }
                     break;
-                case APPIUM_PLATFORM_VERSION:
+                case PLATFORM_VERSION:
                     if (actualValue != null) {
                         // Limited interval: 6.1.1-7.0
                         if (expectedValue.matches("(\\d+\\.){0,}(\\d+)-(\\d+\\.){0,}(\\d+)$")) {
@@ -137,12 +126,12 @@ public class MobileCapabilityMatcher extends DefaultCapabilityMatcher {
                         }
                     }
                     break;
-                case APPIUM_DEVICE_NAME:
+                case DEVICE_NAME:
                     if (actualValue != null && !Arrays.asList(expectedValue.split(",")).contains(actualValue)) {
                         return false;
                     }
                     break;
-                case APPIUM_DEVICE_TYPE:
+                case DEVICE_TYPE:
                     if (actualValue != null && !StringUtils.equalsIgnoreCase(actualValue, expectedValue)) {
                         return false;
                     }
